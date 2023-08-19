75dbdae0da4455ce89a4260d6b5dcc2d13e668c2
Manson <manson972@yahoo.com>
2023-08-19 21:12:34 +0800

    add TodoList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto mt-4 bg-red-400">
+    <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">TODO LIST APP</h1>
         <AddTask />
       </div>
+      <TodoList />
     </main>
   )
 }
