238f7172dcceab690aed01ad4a4c797e361c2231
Manson <manson972@yahoo.com>
2023-08-27 15:12:30 +0800

    add task content in todo list

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import { ITask } from '@/types/tasks';
+import React from 'react';
 
-const TodoList = () => {
+interface TodoListProps {
+  tasks: ITask[]
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table">
       {/* head */}
         <thead className='bg-gray-200'>
           <tr>
-            <th>Name</th>
-            <th>Favorite Color</th>
+            <th>Tasks</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <td>Cy Ganderton</td>
-            <td>Blue</td>
-          </tr>
+          { tasks.map((task) => {
+            return <tr key={task.id}>
+              <td>{task.text}</td>
+            </tr>
+          }) }
         </tbody>
       </table>
     </div>
