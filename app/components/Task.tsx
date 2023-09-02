382c65aecfb7bfd09fe518e457075db16fb5fcd3
Manson <manson972@yahoo.com>
2023-09-03 00:08:32 +0800

    add edit task modal

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,14 +1,35 @@
-import { ITask } from "@/types/tasks"
+import { ITask } from "@/types/tasks";
+import { useState } from "react";
+import { FiEdit, FiTrash2 } from 'react-icons/fi';
+import Modal from "./Modal";
 
 interface TaskProps {
     task: ITask
 }
 
 const Task:React.FC<TaskProps> = ({ task }) => {
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo = () => {}
+
   return (
     <tr key={task.id}>
-        <td>{task.text}</td>
-        <td>Blue</td>
+        <td className="w-full">{task.text}</td>
+        <td className="flex gap-5">
+          <FiEdit cursor="pointer" className="text-blue-500" size={25} />
+          <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Add new task</h3>
+            <div className="modal-action">
+              <input value={taskToEdit} onChange={e => setTaskToEdit(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
+              <button type="submit" className="btn">Submit</button>
+            </div>
+          </form>
+        </Modal>
+          <FiTrash2 cursor="pointer" className="text-red-500" size={25} />
+        </td>
     </tr>
   )
 }
